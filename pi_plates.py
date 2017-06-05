27d16b4557e365df9b9533c026aa3d59a8fce127
Kevin Mahoney <kmahoney40@hotmail.com>
2017-06-05 03:12:33 +0000

    cleanup

--- pi_plates.py
+++ pi_plates.py
@@ -4,11 +4,23 @@ import time
 import pygame
 from pygame.locals import *
 from datetime import datetime
+import pytz
 import curses
 import httplib
 import requests
 import json
 from decimal import Decimal
+
+
+def getDoorCmnd(line):
+    headers = { 'content-type': 'application/json' }
+    fullUrl = url + 'door'
+    stdscr.addstr(line, 0, fullUrl)
+    line += 1
+    ret = requests.get(fullUrl, headers=headers)
+    stdscr.addstr(line, 0, str(ret.status_code) + ": " + str(ret.text) )
+    line += 1
+    return
 
 try:
     stdscr = curses.initscr()
@@ -44,6 +56,8 @@ try:
     line += 1
     line += 1
 
+    headers = { 'content-type': 'application/json' }
+    
     fanOn = False
 
     loopCount = 0
@@ -57,8 +71,14 @@ try:
             loopLine += 1
             continue1 = False
 
-        if loopCount > 9:
+        if loopCount % 2 == 0:
+            getDoorCmnd(line)
+            loopLine += 1
+            loopLine += 1
+
+        if continue1 and loopCount > 9:
             loopCount = 0
+
             tmp1 = 100 * DAQC.getADC(0, 0) - 50
             tmp1 = round(tmp1, 1)
             tmp2 = 100 * DAQC.getADC(0, 1) - 50
@@ -84,18 +104,18 @@ try:
             stdscr.addstr(loopLine, 0, "tmp3: " + str(tmp3))
             loopLine += 1
 
-            payload = {"TEMP_1":tmp1, "TEMP_2":tmp2, "TEMP_3":tmp3, "FAN_ON": fanOn, "VOLTAGE": volts}
+            payload = {"TEMP_1":tmp1, "TEMP_2":tmp2, "TEMP_3":tmp3, "FAN_ON": fanOn, "VOLTAGE": volts, "GMT": str(datetime.now(pytz.utc))}
             stdscr.addstr(loopLine, 0, "payload: " + json.dumps(payload))
             loopLine += 1
 
             stdscr.addstr(loopLine, 0, "state: " + str(RELAY.relaySTATE(0)))
             loopLine += 1
 
-            headers = { 'content-type': 'application/json' }
-            ret = requests.post(url, json=json.dumps(payload), headers=headers)
+            #headers = { 'content-type': 'application/json' }
+            ret = requests.post(url + 'piplates', json=json.dumps(payload), headers=headers)
             stdscr.addstr(loopLine, 0, "return: " + str(ret.status_code) + " " + ret.text)
             loopLine += 1
-            stdscr.addstr(loopLine, 0, "datetime: " + str(datetime.now()))
+            stdscr.addstr(loopLine, 0, "datetime: " + str(datetime.now(pytz.utc)))
             loopLine += 1
         else:
             loopCount += 1
