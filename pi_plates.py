af991534fe4b268ef68ece1e60e69f56e3c0ba43
Kevin Mahoney <kmahoney40@hotmail.com>
2017-11-23 23:05:28 +0000

    clean up

--- pi_plates.py
+++ pi_plates.py
@@ -43,17 +43,15 @@ def getDoorCmnd(line):
 
     except Exception, ex:
         # pring the exception and keep going
-        stdscr.addstr(line + localLine, 0, "getDoorCmnd() outter exception: " + str(ex))
+        stdscr.addstr(line + localLine, 0, "getDoorCmnd() outter exception: " + ex.message)
         localLine += 3
     else:
         # all good do nothing
         stdscr.addstr(line + localLine, 0, "getDoorCmnd() all good!")
         localLine += 3
     finally:
-        # time.sleep(5)
         stdscr.addstr(line + localLine, 0, "END: getDoorCmmd() - " + str(datetime.utcnow()))
         localLine += 3
-        # time.sleep(5)
     return localLine
 
 # END getDoorCmnd() function
@@ -88,16 +86,17 @@ try:
     fanChangeTemp = configJson["fanChangeTemp"]
     fanDelta = configJson["fanDelta"]
     voltageTrigger = configJson["voltageTrigger"]
-    loopTime = configJson["loopTime"]
     alpha = configJson["alpha"]
+    modMin = configJson["modMin"]
+    modSec = configJson["modSec"]
 
     stdscr.addstr(line, 0, "url: " + url)
     line += 1
     stdscr.addstr(line, 0, "fanChangeTemp: " + str(fanChangeTemp))
     line += 1
-    stdscr.addstr(line, 0, "fanDelta: " + str(fanDelta))
+    stdscr.addstr(line, 0, "fanDelta: " + str(fanDelta) + " alpha: " + str(alpha))
     line += 1
-    stdscr.addstr(line, 0, "loopTime: " + str(loopTime))
+    stdscr.addstr(line, 0, "modMin: " + str(modMin) + " modSec: " + str(modSec))
     line += 1
     line += 1
 
@@ -110,12 +109,10 @@ try:
     fTemp2 = 0.0
     fTemp3 = 0.0
     fVolts = 0.0
-    loopCount = 0
+    tmp1 = tmp2 = tmp3 = 0
     timeNow = time.time()
     while continue1 == True:
 
-        #RELAY.relayON(0, 5)
-        
         loopLine = line
 
         timeNow = time.time()
@@ -130,15 +127,14 @@ try:
             loopLine += 1
             continue1 = False
 
-        if loopCount % 3 == 0:
+        myTime = time.localtime(time.time())
+        myMin = myTime.tm_min
+        mySec = myTime.tm_sec
+
+        if mySec % modSec == 0:
             ll = loopLine
-            #stdscr.addstr(50, 0, "loopLine: " + str(loopLine))
             lll = getDoorCmnd(ll)
             loopLine += lll
-            #stdscr.addstr(51, 0, "loopLine: " + str(lll))
-            #loopLine += lll
-            #loopLine += 1
-            #loopLine += 1
 
             tmp1 = 100 * DAQC.getADC(0, 0) - 50
             tmp1 = round(tmp1, 1)
@@ -147,20 +143,13 @@ try:
             tmp3 = 100 * DAQC.getADC(0, 2) - 50
             tmp3 = round(tmp3, 1)
             volts = DAQC.getADC(0, 3)
-            stdscr.addstr(loopLine, 0, "inside loopCount loopLine: " + str(loopLine))
+            stdscr.addstr(loopLine, 0, "inside call getDoorCmd() if - mySec: " + str(mySec))
             loopLine += 1
-            loopCount = 0
         else:
             loopLine += 12
-            stdscr.addstr(loopLine, 0, "in loopCount else: " + str(loopLine) + "          ")
+            stdscr.addstr(loopLine, 0, "in else no getDoorCmd() - mySec: " + str(mySec) + "          ")
             loopLine += 1
-        loopCount += 1
-        stdscr.addstr(loopLine, 0, "rawTmp1: " + str(tmp1))
-        loopLine += 1
-        stdscr.addstr(loopLine, 0, "rawTmp2: " + str(tmp2))
-        loopLine += 1
-        stdscr.addstr(loopLine, 0, "rawTmp3: " + str(tmp3))
-        loopLine += 1
+
 
         fTemp1 = (1 - alpha) * tmp1 + alpha * fTemp1
         fTemp1 = round(fTemp1, 1)
@@ -174,81 +163,72 @@ try:
         fVolts = (1 - alpha) * volts + alpha * fVolts
         fVolts = round(fVolts, 1)
 
-        stdscr.addstr(loopLine, 0, "filteredTmp1: " + str(fTemp1))
+        stdscr.addstr(loopLine, 0, "rawTmp1: " + str(tmp1) + " filteredTmp1: " + str(fTemp1))
         loopLine += 1
-        stdscr.addstr(loopLine, 0, "filteredTmp2: " + str(fTemp2))
+        stdscr.addstr(loopLine, 0, "rawTmp2: " + str(tmp2) + " filteredTmp2: " + str(fTemp2))
         loopLine += 1
-        stdscr.addstr(loopLine, 0, "filteredTmp3: " + str(fTemp3))
+        stdscr.addstr(loopLine, 0, "rawTmp3: " + str(tmp3) + " filteredTmp3: " + str(fTemp3))
         loopLine += 1
 
         loopLine += 2
-        #if continue1 and 
-        myTime = time.localtime(time.time()) 
-        myMin = myTime.tm_min
         stdscr.addstr(loopLine, 0, "min: " + str(myMin) + " mod: " + str(myMin % 5) + " tempfanCounter: " + str(tempFanCounter) )
         loopLine += 2
 
-        if myMin % 15 == 0 and tempFanCounter == 0:
-            tempFanCounter = 1
-            stdscr.addstr(loopLine, 0, "tempFanCounter inside" + str(tempFanCounter) + "          ")
-            loopLine += 1
-
-            strtmp1 = str(fTemp1)
-            strtmp2 = str(fTemp2)
-            strtmp3 = str(fTemp3)
-
-            stdscr.addstr(loopLine, 0, "sentTmp1: " + strtmp1)
-            loopLine += 1
-            stdscr.addstr(loopLine, 0, "sentTmp2: " + strtmp2)
-            loopLine += 1
-            stdscr.addstr(loopLine, 0, "sentTmp3: " + strtmp3)
-            loopLine += 1
-
-            if fVolts < voltageTrigger:
-                RELAY.relayON(0, 7)
-                chargerOn = True
-            else:
-                RELAY.relayOFF(0, 7)
-                chargerOn = False
-
-            if fTemp1 > fanChangeTemp + fanDelta:
-                RELAY.relayON(0, 3)
-                fanOn = True
-            elif fTemp1 < fanChangeTemp - fanDelta:
-                RELAY.relayOFF(0, 3)
-                fanOn = False
-
-            stdscr.addstr(loopLine, 0, "tmp1: " + strtmp1)
-            loopLine += 1
-            stdscr.addstr(loopLine, 0, "tmp2: " + strtmp2)
-            loopLine += 1
-            stdscr.addstr(loopLine, 0, "tmp3: " + strtmp3)
-            loopLine += 1
-
-            payload = {"TEMP_1": fTemp1, "TEMP_2": fTemp2, "TEMP_3": fTemp3, "FAN_ON": fanOn, "CHARGER_ON": chargerOn,
-                       "VOLTAGE": fVolts, "GMT": str(datetime.utcnow())}
-            # str(datetime.now(pytz.utc))}
-            stdscr.addstr(loopLine, 0, "payload: " + json.dumps(payload))
-            loopLine += 2
-
-            stdscr.addstr(loopLine, 0, "state: " + str(RELAY.relaySTATE(0)))
-            loopLine += 1
-
-            headers = { 'content-type': 'application/json' }
+        if myMin % modMin == 0 and tempFanCounter == 0:
             try:
-                var = 2
+                tempFanCounter = 1
+                stdscr.addstr(loopLine, 0, "tempFanCounter inside" + str(tempFanCounter) + "          ")
+                loopLine += 1
+
+                strtmp1 = str(fTemp1)
+                strtmp2 = str(fTemp2)
+                strtmp3 = str(fTemp3)
+
+                stdscr.addstr(loopLine, 0, "sentTmp1: " + strtmp1)
+                loopLine += 1
+                stdscr.addstr(loopLine, 0, "sentTmp2: " + strtmp2)
+                loopLine += 1
+                stdscr.addstr(loopLine, 0, "sentTmp3: " + strtmp3)
+                loopLine += 1
+
+                if fVolts < voltageTrigger:
+                    RELAY.relayON(0, 7)
+                    chargerOn = True
+                else:
+                    RELAY.relayOFF(0, 7)
+                    chargerOn = False
+
+                if fTemp1 > fanChangeTemp + fanDelta:
+                    RELAY.relayON(0, 3)
+                    fanOn = True
+                elif fTemp1 < fanChangeTemp - fanDelta:
+                    RELAY.relayOFF(0, 3)
+                    fanOn = False
+
+                payload = {"TEMP_1": fTemp1, "TEMP_2": fTemp2, "TEMP_3": fTemp3, "FAN_ON": fanOn, "CHARGER_ON": chargerOn,
+                           "VOLTAGE": fVolts, "GMT": str(datetime.utcnow())}
+                stdscr.addstr(loopLine, 0, "payload: " + json.dumps(payload))
+                loopLine += 2
+
+                stdscr.addstr(loopLine, 0, "state: " + str(RELAY.relaySTATE(0)))
+                loopLine += 1
+            except Exception, ex:
+                stdscr.addstr(loopLine, 0, "Exception: " + ex.message)
+            finally:
+                var = 3
+            try:
+                headers = {'content-type': 'application/json'}
                 ret = requests.post(url + 'piplates', json=json.dumps(payload), headers=headers)
                 stdscr.addstr(loopLine, 0, "return: " + str(ret.status_code) + " " + ret.text)
             except Exception, ex:
                 loopLine += 1
-                #stdscr.addstr(loopLine, 0, "requests.post exception: " + ex.message)
                 loopLine += 1
-                stdscr.addstr(loopLine, 0, "Exception: " + str(ex))
+                stdscr.addstr(loopLine, 0, "Exception: " + ex.message)
                 loopLine += 1
             else:
-                loopLine += 1
-                stdscr.addstr(loopLine, 0, "All good")
                 loopLine += 2
+                stdscr.addstr(loopLine, 0, "Send temps all good")
+                loopLine += 1
             finally:
                 loopLine += 1
                 stdscr.addstr(loopLine, 0, "datetime: " + str(datetime.utcnow()))
@@ -258,86 +238,12 @@ try:
                 tempFanCounter += 1
                 if tempFanCounter > 60:
                     tempFanCounter = 0
-#                tempFanCounter += 1
             stdscr.addstr(loopLine, 0, "tempFanCounter else: " + str(tempFanCounter) + "                    ")
             loopLine += 1
 
         loopLine += 1
         time.sleep(1)
 
-        # if continue1 and loopCount > loopTime:
-        #     loopCount = 0
-
-            #itmp1 = 100 * DAQC.getADC(0, 0) - 50
-            #tmp1 = round(tmp1, 1)
-            #tmp2 = 100 * DAQC.getADC(0, 1) - 50
-            #tmp2 = round(tmp2, 1)
-            #tmp3 = 100 * DAQC.getADC(0, 2) - 50
-            #tmp3 = round(tmp3, 1)
-            #volts = DAQC.getADC(0, 3)
-#             strtmp1 = str(fTemp1)
-#             strtmp2 = str(fTemp2)
-#             strtmp3 = str(fTemp3)
-#
-#             stdscr.addstr(loopLine, 0, "sentTmp1: " + strtmp1)
-#             loopLine += 1
-#             stdscr.addstr(loopLine, 0, "sentTmp2: " + strtmp2)
-#             loopLine += 1
-#             stdscr.addstr(loopLine, 0, "sentTmp3: " + strtmp3)
-#             loopLine += 1
-#
-#             if fVolts < voltageTrigger:
-#                 RELAY.relayON(0,7)
-#                 chargerOn = True
-#             else:
-#                 RELAY.relayOFF(0,7)
-#                 chargerOn = False
-#
-#
-#             if fTemp1 > fanChangeTemp + fanDelta:
-#                 RELAY.relayON(0,3)
-#                 fanOn = True
-#             elif fTemp1 < fanChangeTemp - fanDelta:
-#                 RELAY.relayOFF(0,3)
-#                 fanOn = False
-#
-# #            stdscr.addstr(loopLine, 0, "tmp1: " + strtmp1)
-# #            loopLine += 1
-# #            stdscr.addstr(loopLine, 0, "tmp2: " + strtmp2)
-# #            loopLine += 1
-# #            stdscr.addstr(loopLine, 0, "tmp3: " + strtmp3)
-# #            loopLine += 1
-#
-#             payload = {"TEMP_1":fTemp1, "TEMP_2":fTemp2, "TEMP_3":fTemp3, "FAN_ON": fanOn, "CHARGER_ON": chargerOn, "VOLTAGE": fVolts, "GMT": str(datetime.utcnow())}
-# #str(datetime.now(pytz.utc))}
-#             stdscr.addstr(loopLine, 0, "payload: " + json.dumps(payload))
-#             loopLine += 2
-#
-#             stdscr.addstr(loopLine, 0, "state: " + str(RELAY.relaySTATE(0)))
-#             loopLine += 1
-#
-#             #headers = { 'content-type': 'application/json' }
-#             try:
-#                 ret = requests.post(url + 'piplates', json=json.dumps(payload), headers=headers)
-#                 stdscr.addstr(loopLine, 0, "return: " + str(ret.status_code) + " " + ret.text)
-#             except Exception, ex:
-#                 loopLine += 1
-#                 stdscr.addstr(loopLine, 0, "request.post : " + payload + " exception.")
-#                 loopLine += 1
-#                 stdscr.addstr(loopLine, 0, "Exception: " + str(ex))
-#                 loopLine += 1
-#             else:
-#                 loopLine += 1
-#                 stdscr.addstr(loopLine, 0, "All good")
-#                 loopLine += 2
-#             finally:
-#                 loopLine += 1
-#                 stdscr.addstr(loopLine, 0, "datetime: " + str(datetime.now(pytz.utc)))
-#                 loopLine += 2
-#         else:
-#             loopCount += 1
-
-        #time.sleep(1)
 finally:
     curses.endwin()
 
